37c024664910d96d2d6e2863cd5085db355d3782
c-dez <hdezcruzcarlos@gmail.com>
2023-08-12 23:57:43 -0600

    added eventListener to button, displays temperature in C

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//38a7954fa4704ec8afb25426230908
+//38a7954fa4704ec8afb25426230908 BORRAR!
 
 
 const container = document.querySelector('#container')
@@ -11,20 +11,25 @@ async function getWeather(){
         {mode: 'cors'} );
     response.json().then(function(response) {
         console.table(response)
-        test.textContent = response.current.temp_c
+        dataDisplay.textContent = response.current.temp_c +' C'
     })    
 }
-// getWeather()
-const btn = document.querySelector('button')
-const imgFace = "url('./img/sayuFace.jpg')"
 
-btn.style.background = imgFace
+
+const btn = document.querySelector('button')
+btn.style.background =  "url('./img/sayuFace.jpg')"
 btn.style.backgroundSize = 'contain'
 btn.style.height = '200px'
 btn.style.width = '200px'
 
+btn.addEventListener('click', ()=>{
+    getWeather() 
+})
 
-
+//data
+const data = document.querySelector('.dataContainer')
+const dataDisplay = document.querySelector('.tempDisplay')
+data.appendChild(dataDisplay)
 
 
 
